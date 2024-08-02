d87101decb68ba05a857a36a92efb09261cbdf29
Apostolos Ntarampekis <apntaramp@gmail.com>
2024-08-03 01:06:21 +0300

    Add test suite for stations endpoints

--- src/weather_app/common/tests/config.py
+++ src/weather_app/common/tests/config.py
@@ -2,6 +2,8 @@
 auth_url = "/api/v1/auth/"
 users_url = "/api/v1/users/"
 user_url = "/api/v1/users/{user_uuid}/"
+stations_url = "/api/v1/stations/"
+station_url = "/api/v1/stations/{station_uuid}/"
 
 # post/patch default arguments
 default_post_args = {"content_type": "application/json"}
