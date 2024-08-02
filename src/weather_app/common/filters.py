2179a3dce6f475cdf35717b49fc62174b4d93f11
Apostolos Ntarampekis <apntaramp@gmail.com>
2024-08-03 00:16:11 +0300

    Add base filter for filtering by range

---
+++ src/weather_app/common/filters.py
@@ -0,0 +1,38 @@
+import logging
+
+from rest_framework.filters import BaseFilterBackend
+
+logger = logging.getLogger(__name__)
+
+
+class RangeFilter(BaseFilterBackend):
+    """
+    Abstract filter class for filtering by range.
+    Concrete classes need only to provide the filtering field and the query param.
+    """
+
+    field = None
+    query_param = None
+
+    def filter_queryset(self, request, queryset, view):
+        range_query = request.query_params.get(self.query_param)
+
+        if range_query is None:
+            return queryset
+
+        try:
+            min_value = float(range_query.strip("[]()").split(",")[0])
+            max_value = float(range_query.strip("[]()").split(",")[1])
+            if min_value >= max_value:
+                raise ValueError(
+                    f"Min value must be smaller that max value in {self.field} range."
+                )
+        except (ValueError, IndexError):
+            logger.warning(
+                f"Badly formed {self.field} range: {range_query}."
+                f"Two comma-separated values must be provided."
+            )
+            return queryset.none()
+
+        filter_query = {f"{self.field}__range": (min_value, max_value)}
+        return queryset.filter(**filter_query)
