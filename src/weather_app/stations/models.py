f3bfbd6ed11c92e52e42aa6a97917ae4e54ac325
Apostolos Ntarampekis <apntaramp@gmail.com>
2024-08-03 01:47:12 +0300

    Use django setting to get user model for one-to-one relation in station model

--- src/weather_app/stations/models.py
+++ src/weather_app/stations/models.py
@@ -1,12 +1,13 @@
 from uuid import uuid4
 
+from django.conf import settings
 from django.db import models
-
-from weather_app.users.models import User
 
 
 class Station(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=False)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, primary_key=False
+    )
     uuid = models.UUIDField(unique=True, default=uuid4)
     name = models.CharField(unique=True, max_length=50)
     latitude = models.DecimalField(max_digits=8, decimal_places=6)
